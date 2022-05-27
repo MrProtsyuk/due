6020c94e3bbc3d0ab299a3cda8822168bdbcbcbc
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-26 20:47:24 -0700

    created mutations and queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,71 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    {
+        me {
+            _id
+            username
+            email
+            userCategory {
+                _id
+                name
+            }
+            userExpenses {
+                _id
+                description
+                date
+                amount
+                link
+                category
+            }
+        }
+    }
+`;
+
+export const QUERY_CATEGORIES = gql`
+    query categories($name: String!) {
+        categories(name: $name) {
+            _id
+            name
+        }
+    }
+`;
+
+export const QUERY_USER  = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            userCategory
+            userExpenses
+        }
+    }
+`;
+
+export const QUERY_EXPENSES  = gql`
+    query expenses($username: String) {
+        expenses(username: $username) {
+            _id
+            description
+            date
+            amount
+            link
+            category
+        }
+
+    }
+`;
+
+export const QUERY_EXPESNE  = gql`
+    query expense($id: ID!){
+        expense(_id: $id) {
+            _id
+            description
+            date
+            amount
+            link
+            category
+        }
+    }
+`;
