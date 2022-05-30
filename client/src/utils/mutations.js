81fba192d3c8d79391fa41bbfaf0c522c138a4df
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-29 17:00:21 -0700

    add file links in app js and minor typo fixes

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN_USER  = gql`
 `;
 
 export const ADD_USER  = gql`
-    mutation addUser($username: String!, $emai: String!, $password: String!) {
+    mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
             token 
             user {
