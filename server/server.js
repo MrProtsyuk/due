1eed9f0f5848de03d43fbea83790d9e2cdb7ba2c
Ridercogswell <ridercogswell@gmail.com>
2022-06-01 00:43:56 -0700

    change to build

--- server/server.js
+++ server/server.js
@@ -30,12 +30,12 @@ const startApolloServer = async (typeDefs, resolvers) => {
 //app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/public')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 // Mark: Edited this, if it doesnt work then comment it out!
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/public/index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 db.once('open', () => {
